652529644be7af3a9a71377147b0a97188e31b33
“Matej <matej.filkovic@gmail.com>
2018-09-10 21:08:28 +0200

    Remove unused exports from lunchr

--- lunchhr_slack_command/lunchhr/pages/__init__.py
+++
@@ -1,5 +0,0 @@
-from .user import User
-from .base_page import BasePage
-from .order_overview_page import OrderOverviewPage
-from .pick_meal_page import PickMealPage
-from .order_page import OrderPage
